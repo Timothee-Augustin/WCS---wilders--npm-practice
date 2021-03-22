ddbeee5c75fae30c99333d73d90d82f759337b64
Timothee <augustin.timothee@gmail.com>
2021-03-22 15:24:15 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const chalk = require("chalk");
+
+console.log(chalk.blue("Timothee"));
+console.log(chalk.green("Cyril"));
+console.log(chalk.yellow("Brian"));
+console.log(chalk.red("TotoElFuego"));
